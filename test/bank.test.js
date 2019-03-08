df50869b73b2c8e7903daacc63c8f14afab117ba
Lu Xie <pgxlxianjian@gmail.com>
2019-03-08 14:00:40 +0800

    update

--- test/bank.test.js
+++ test/bank.test.js
@@ -1,9 +1,13 @@
 var expect = require('chai').expect;
 var axios = require('axios');
+var fs = require("fs");
+var url = fs.readFileSync('test/testdata.json');
+url = String(url);
+console.log(url);
 
 describe('/bank endpoint test', function() {
   it('check payment_method for LOCAL', function(done) {
-    axios.post('http://localhost:30001/bank', {
+    axios.post(url, {
       "payment_method":"LOCAL","bank_country_code":"US","account_name":"John Smith","account_number":"123","aba":"11122233A"
     })
     .then(function (response) {
@@ -16,7 +20,7 @@ describe('/bank endpoint test', function() {
   });
 
   it('check payment_method for SWIFT', function(done) {
-      axios.post('http://localhost:30001/bank', {
+      axios.post(url, {
         "payment_method":"SWIFT","bank_country_code":"US","account_name":"John Smith","account_number":"123","aba":"11122233A"
       })
       .then(function (response) {
@@ -29,7 +33,7 @@ describe('/bank endpoint test', function() {
   });
 
   it('unknown payment_method', function(done) {
-      axios.post('http://localhost:30001/bank', {
+      axios.post(url, {
         "payment_method":"XXXXX","bank_country_code":"US","account_name":"John Smith","account_number":"123","swift_code":"ICBCUSBJ","aba":"11122233A"
       })
       .then(function (response) {
@@ -43,7 +47,7 @@ describe('/bank endpoint test', function() {
 
 
   it('check unknown country', function(done) {      
-      axios.post('http://localhost:30001/bank', {
+      axios.post(url, {
         "payment_method":"XXXXX","bank_country_code":"ZZ","account_name":"John Smith","account_number":"123","swift_code":"ICBCUSBJ","aba":"11122233A"
       })
       .then(function (response) {
@@ -55,7 +59,7 @@ describe('/bank endpoint test', function() {
   });
 
   it('check country for US', function(done) {
-      axios.post('http://localhost:30001/bank', {
+      axios.post(url, {
         "payment_method":"LOCAL","bank_country_code":"AU","account_name":"Pokeman Smith","account_number":"123","swift_code":"ICBCUSBJ","aba":"11122233A"
       })
       .then(function (response) {
@@ -67,7 +71,7 @@ describe('/bank endpoint test', function() {
   });
 
   it('check country for AU', function(done) {
-      axios.post('http://localhost:30001/bank', {
+      axios.post(url, {
         "payment_method":"LOCAL","bank_country_code":"AU","account_name":"Pokeman Smith Picachu picapicapicapica","account_number":"123","swift_code":"ICBCUSBJ","aba":"11122233A"
       })
       .then(function (response) {
